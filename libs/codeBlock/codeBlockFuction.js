b02f71abc7a001200c10f0651ec6d6d8c9d4281e
zhujie-ww <1282204281@qq.com>
2020-04-10 22:15:09 +0800

    Site updated: 2020-04-10 22:15:02

--- libs/codeBlock/codeBlockFuction.js
+++ libs/codeBlock/codeBlockFuction.js
@@ -1,5 +1,3 @@
-// 代码块功能依赖
-
-$(function () {
-    $('pre').wrap('<div class="code-area" style="position: relative"></div>');
-});
+// build time:Fri Apr 10 2020 22:14:57 GMT+0800 (GMT+08:00)
+$(function(){$("pre").wrap('<div class="code-area" style="position: relative"></div>')});
+//rebuild by neat 
